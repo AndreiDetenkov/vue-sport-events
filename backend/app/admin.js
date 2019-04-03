15884c44f4e9c0da9679984815b652e3d1f1ec1f
Andrei Detenkov <a.detenkov@gmail.com>
2019-04-03 22:37:31 +0600

    #22-backend: find event by id
    - created endpoint for response event info if client send event id on backend.
    - reformatted code with prettier.

--- backend/app/admin.js
+++ backend/app/admin.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const multer = require('multer');
-const nanoid = require('nanoid');
-const path = require('path');
-const fs = require('fs-extra');
-const config = require('../config');
-const Event = require('../models/Event');
-const auth = require('../middlewares/auth');
+const express = require('express')
+const multer = require('multer')
+const nanoid = require('nanoid')
+const path = require('path')
+const fs = require('fs-extra')
+const config = require('../config')
+const Event = require('../models/Event')
+const auth = require('../middlewares/auth')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const dirName = req.body.dirId;
-    fs.mkdirsSync(config.uploadPath + `/${dirName}`);
-    cb(null, config.uploadPath + `/${dirName}`);
+    const dirName = req.body.dirId
+    fs.mkdirsSync(config.uploadPath + `/${dirName}`)
+    cb(null, config.uploadPath + `/${dirName}`)
   },
   filename: (req, file, cb) => {
-    cb(null, nanoid() + path.extname(file.originalname));
+    cb(null, nanoid() + path.extname(file.originalname))
   }
-});
+})
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
 const createRouter = () => {
-  const router = express.Router();
+  const router = express.Router()
 
   // const uploadFiles = upload.fields([
   //   { name: 'imagePreview', maxCount: 1 }, { name: 'images', maxCount: 10 }
@@ -29,25 +29,19 @@ const createRouter = () => {
 
   // -----------------  сохранение нового эвента в БД  ---------------------
   router.post('/add-event-preview', [auth, upload.single('imagePreview')], async (req, res) => {
-    // Object.keys(req.body).forEach(item => {
-    //   if (req.body[item] === '') {
-    //     res.status(400).send({ message: 'Все поля должны быть заполнены!' })
-    //   }
-    // });
-
     // console.log('req: ', req.body)
 
-    const data = {};
+    const data = {}
 
     for (let el in req.body) {
       data[el] = req.body[el]
     }
-    delete data.lat;
-    delete data.lng;
+    delete data.lat
+    delete data.lng
 
     if (req.file) {
       data.imagePreview = req.file.filename
-    } else res.status(400).send({ message: 'Не добавлена фотография!' });
+    } else res.status(400).send({ message: 'Не добавлена фотография!' })
 
     // try {
     //   if (Object.keys(req.files).length === 2) {
@@ -62,68 +56,80 @@ const createRouter = () => {
     //   });
     // }
 
-    data.gps = [req.body.lat, req.body.lng];
+    data.gps = [req.body.lat, req.body.lng]
 
-    const event = new Event(data);
+    const event = new Event(data)
 
     try {
-      const title = await Event.findOne({ title: data.title });
+      const title = await Event.findOne({ title: data.title })
       if (title) {
         res.status(400).send({
           message: `Название - ${data.title} уже есть в БД!`
-        });
+        })
       }
 
-      const newEvent = await event.save();
+      const newEvent = await event.save()
       if (newEvent) {
         res.status(200).send({
           message: `Эвент - ${data.title} успешно добавлен в БД`,
           event: newEvent
-        });
+        })
       }
     } catch (e) {
       res.status(400).send({
-        message: 'Ошибка при добавлении записи в БД', error: e.message
-      });
+        message: 'Ошибка при добавлении записи в БД',
+        error: e.message
+      })
     }
-  });
+  })
 
   // -----------------  получение списка всех эвентов ---------------------
-  router.get('/events/list', auth, async (req, res) => {
-    const events = await Event.find({}).sort({ date: 1 });
-    if (events.length > 0) res.status(200).send(events);
+  router.get('/events/list', async (req, res) => {
+    const events = await Event.find({}).sort({ date: 1 })
+    if (events.length > 0) res.status(200).send(events)
     else res.status(200).send({ events, message: 'Нет эвентов в БД!' })
-  });
+  })
 
   // -----------------  удаление эвента по id ---------------------
   router.delete('/event/delete/:id', auth, async (req, res) => {
-    const event = await Event.deleteOne({ _id: req.params.id });
-    const dirName = req.body.dirId;
-    const dirPath = config.uploadPath + `/${dirName}`;
-
-    const deleteFolderRecursive = (path) => {
-      if (fs.existsSync(path)) {
-        fs.readdirSync(path).forEach((file, index) => {
-          let curPath = path + '/' + file;
-          if (fs.lstatSync(curPath).isDirectory()) {
-            deleteFolderRecursive(curPath);
-          } else {
-            fs.unlinkSync(curPath);
-          }
-        });
-        fs.rmdirSync(path);
-      }
-    };
-    deleteFolderRecursive(dirPath);
-
     try {
-      if (event) res.send({ message: `Эвент ${event.name} успешно удален` });
+      const event = await Event.deleteOne({ _id: req.params.id })
+      if (event) res.send({ message: `Эвент ${event.name} успешно удален` })
+
+      const dirName = req.body.dirId
+      const dirPath = config.uploadPath + `/${dirName}`
+
+      const deleteFolderRecursive = path => {
+        if (fs.existsSync(path)) {
+          fs.readdirSync(path).forEach((file, index) => {
+            let curPath = path + '/' + file
+            if (fs.lstatSync(curPath).isDirectory()) {
+              deleteFolderRecursive(curPath)
+            } else {
+              fs.unlinkSync(curPath)
+            }
+          })
+          fs.rmdirSync(path)
+        }
+      }
+      deleteFolderRecursive(dirPath)
     } catch (error) {
-      return res.send({ message: 'Ошибка при удалении', error });
+      return res.send({ message: 'Ошибка при удалении', error })
     }
-  });
+  })
 
-  return router;
-};
+  // -----------------  получени эвента по id для редактирования ---------------------
+  router.get('/event/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+      const event = await Event.findById(id)
+      if (event) res.send(event)
+    } catch (error) {
+      res.status(404).send({ message: `Не найдено ни одного эвента с id: ${id}` })
+    }
+  })
 
-module.exports = createRouter;
+  return router
+}
+
+module.exports = createRouter
--- backend/app/events.js
+++ backend/app/events.js
@@ -1,26 +1,24 @@
-const express = require('express');
-const Event = require('../models/Event');
+const express = require('express')
+const Event = require('../models/Event')
 
 const createRouter = () => {
-  const router = express.Router();
+  const router = express.Router()
 
   router.get('/list', async (req, res) => {
-    const events = await Event.find({}).sort({ date: 1 });
-    if (events.length > 0) res.status(200).send(events);
+    const events = await Event.find({}).sort({ date: 1 })
+    if (events.length > 0) res.status(200).send(events)
     else res.status(200).send({ events, message: 'Нет эвентов в БД!' })
-  });
+  })
 
   router.get('/item/:id', async (req, res) => {
     const event = await Event.findById(req.params.id)
-    if (event) res.status(200).send(event);
+    if (event) res.status(200).send(event)
     else res.status(404).send({ message: 'Не найдено ни одного эвента!' })
-  });
+  })
 
-  router.post('/', async (req, res) => {
+  router.post('/', async (req, res) => {})
 
-  });
+  return router
+}
 
-  return router;
-};
-
-module.exports = createRouter;
+module.exports = createRouter
